62fbec64d5a4e42c68e3537df76017e1fb806e51
vivekvadluri01 <142223864+vivekvadluri01@users.noreply.github.com>
2023-09-02 18:35:26 +0530

    changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll",function()
 {
-    let form = document.querySelector(".enquiry");
+    let form = document.getElementById("enquiry");
     if (window.scrollY<=1450)
     {
         form.classList.remove('sticky');
