41bbe0c44972c5f38d2081564de90f09ed7afc62
vivekvadluri01 <142223864+vivekvadluri01@users.noreply.github.com>
2023-09-13 01:28:07 +0530

    done changes

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-var min=document.getElementById("click1");
+function toggleMinBox() {
+    var minBox = document.getElementById("minBox");
+    var maxBox = document.getElementById("maxBox");
 
+    if (minBox.style.display === "block") {
+        minBox.style.display = "none";
+    } else {
+        minBox.style.display = "block";
+        maxBox.style.display = "none";
+    }
+}
 
+function toggleMaxBox() {
+    var minBox = document.getElementById("minBox");
+    var maxBox = document.getElementById("maxBox");
 
+    if (maxBox.style.display === "block") {
+        maxBox.style.display = "none";
+    } else {
+        maxBox.style.display = "block";
+        minBox.style.display = "none";
+    }
+}
 
+function setValue(inputId, selectedValue) {
+    var inputField = document.getElementById(inputId);
+    inputField.value = selectedValue;
+}
 
 
 
